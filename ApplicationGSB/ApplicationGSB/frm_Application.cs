f70c9f1106159f0d58f68c95b8b011cbee69aa07
VALLAS Louis <louis.vallas@icof.fr>
2017-11-30 13:53:18 +0100

    edit btnClick

--- ApplicationGSB/ApplicationGSB/frm_Application.cs
+++ ApplicationGSB/ApplicationGSB/frm_Application.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
 
 namespace ApplicationGSB
 {
@@ -18,35 +17,10 @@ namespace ApplicationGSB
             InitializeComponent();
         }
 
-        #region Variable globales
-        SqlConnection maConnexion;
-        SqlDataAdapter monDataAdapter;
-        SqlCommand maCommand;
-        #endregion
-
         private void btnCreer_Click(object sender, EventArgs e)
         {
-            maConnexion.Open();
-            try
-            {
-
-                string requete = "INSERT INTO \"livre-LV\" (idProduit,effetTherapeutique,nomProduit,contreIndication,pxVenteHT,pxEchantillon,dosage,presentation,idFamille)";
-                requete = requete + "VALUES ('" + txbIdProduit.Text + "','" + txbEffetsTherapeu + "','" + cbxMedicaments.Text + "','" + txtbContreInd.Text + "','" + txtbPxVenteHT.Text + "','" + txtbPxEchantillon.Text + "','" + txtbDosage.Text + "','" + txtbPresentation + "','" + txtbFamille.Text +"';'";
-
-                maCommand = new SqlCommand();
-                maCommand.CommandText = requete;
-                maCommand.Connection = maConnexion;
-
-                maCommand.ExecuteNonQuery();
-                maConnexion.Close();
-                maCommand.Cancel();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-                maConnexion.Close();
-
-            }
+           string requete = "INSERT INTO \"livre-LV\" (idProduit,effetTherapeutique,nomProduit,contreIndication,pxVenteHT,pxEchantillon,dosage,presentation,idFamille)";
+           requete = requete + "VALUES ('" + txbIdProduit.Text + "','" + txbEffetsTherapeu + "','" + cbxMedicaments.Text + "','" + txtbContreInd.Text + "','" + txtbPxVenteHT.Text + "','" + txtbPxEchantillon.Text + "','" + txtbDosage.Text + "','" + txtbPresentation + "','" + txtbFamille.Text +"';'";
 
         }
     }
